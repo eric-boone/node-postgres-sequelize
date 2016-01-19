01225858fec73a12e4ae5e3c29ae1eb40a12f0ff
eric-bone <therealeric@gmail.com>
2016-01-18 17:57:11 -0700

    stuck

--- server/models/todo.js
+++ server/models/todo.js
@@ -9,7 +9,7 @@ module.exports = function(sequelize, DataTypes) {
   }, {
     classMethods: {
       associate: function(models) {
-        Todo.belongsTo(model.User);
+        Todo.belongsTo(models.User);
       }
     }
   });
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models/index');
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/users', function(req, res){
+  models.User.create({
+    email: req.body.email
+  }).then(function(user){
+    res.json(todo);
+  });
 });
 
 module.exports = router;
